48b243c64cca00e073aab711eea63c7b9d7d9016
Matthew Tuttle <matu8568@colorado.edu>
2023-04-04 18:02:44 +0000

    fixed </td>

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ def selecting():
     records = cur.fetchall()
     conn.close()
     response_string=""
-    response_string+="<table width=30%>"
+    response_string+="<table>"
     for player in records:
         response_string+="<tr>"
         for info in player:
-            response_string+="<td>{}</td".format(info)
+            response_string+="<td>{}</td>".format(info)
         response_string+="</tr>"
     response_string+="</table>"
     return response_string
