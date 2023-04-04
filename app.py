076124ffff5c20c6ce6a77cc7dc5eeea72e1fe95
Matthew Tuttle <matu8568@colorado.edu>
2023-04-04 17:15:07 +0000

    bugfix

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def selecting():
     conn.close()
     response_string=""
     response_string+="<table>"
-    for player in records;
+    for player in records:
         response_string+="<tr>"
         for info in player:
             response_string+="<td>{}</td".format(info)
