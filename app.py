d25cd9170996939d3d190ee90ed280c5e8223893
Matthew Tuttle <matu8568@colorado.edu>
2023-04-04 16:38:46 +0000

    fixed db_test route func name

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/db_test')
-def hello_world():
+def test_db():
     conn = psycopg2.connect("postgres://mstdata_user:dmHp2k2RlRARbjkj5pFR3BMpjzd0KltZ@dpg-cgm4v1o7oslael757erg-a/mstdata")
     conn.close()
     return 'Database Connection Successful'
